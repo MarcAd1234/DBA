9e7735f2732015f82d26e2e62dbbc1e7afba81c5
MarcAd1234 <marc-adrian@gmx.net>
2020-04-16 20:18:15 +0200

    Erster Versuch Rechner

---
+++ www/pages/alone.js
@@ -0,0 +1,70 @@
+function ClickButton0() {
+
+    if (s = 1) {
+        erg = erg + 0
+        s = s + 1
+        document.getElementById("s1").innerHTML = "0"
+      } else if (s = 2) {
+        erg = erg + 0
+        s = s + 1
+        document.getElementById("s2").innerHTML = "0"
+      } else if (s = 3){
+        erg = erg + 0
+        s = s + 1
+        document.getElementById("s3").innerHTML = "0"
+      } else if (s = 4){
+        erg = erg + 0
+        s = s + 1
+        document.getElementById("s4").innerHTML = "0"
+      } else if (s = 5){
+        erg = erg + 0
+        s = s + 1
+        document.getElementById("s5").innerHTML = "0"
+      } else if (s = 6){
+        erg = erg + 0
+        s = 1
+        document.getElementById("s6").innerHTML = "0"
+      }
+    }
+}
+
+
+function ClickButton1() {
+
+    if (s = 1) {
+        erg = erg + 1
+        s = s + 1
+        document.getElementById("s1").innerHTML = "1"
+      } else if (s = 2) {
+        erg = erg + 1
+        s = s + 1
+        document.getElementById("s2").innerHTML = "1"
+      } else if (s = 3){
+        erg = erg + 1
+        s = s + 1
+        document.getElementById("s3").innerHTML = "1"
+      } else if (s = 4){
+        erg = erg + 1
+        s = s + 1
+        document.getElementById("s4").innerHTML = "1"
+      } else if (s = 5){
+        erg = erg + 1
+        s = s + 1
+        document.getElementById("s5").innerHTML = "1"
+      } else if (s = 6){
+        erg = erg + 1
+        s = 1
+        document.getElementById("s6").innerHTML = "1"
+      }
+    }
+}
+
+
+
+
+
+
+
+// Werte bei Rundenstart
+  s = 1;
+  erg = 0;
